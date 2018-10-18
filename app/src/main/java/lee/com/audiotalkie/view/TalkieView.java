89f53c370167f257b7549975b7cf588c5ba697db
leeg <leeg@ligang.com>
2018-10-18 15:56:37 +0800

    mvp脚手架

---
+++ app/src/main/java/lee/com/audiotalkie/view/TalkieView.java
@@ -0,0 +1,11 @@
+package lee.com.audiotalkie.view;
+
+import lee.com.audiotalkie.presenter.TalkiePresenter;
+
+/**
+ * CreateDate：18-10-18 on 下午3:46
+ * Describe:
+ * Coder: lee
+ */
+public interface TalkieView extends BaseView<TalkiePresenter>{
+}
